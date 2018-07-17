4537dad68334ae85986fa2c88c4549a2f4db7a32
杜文 <244834793@qq.com>
2018-07-17 16:56:06 +0800

    cash

--- YB-YH/其它/AppDelegate.m
+++ YB-YH/其它/AppDelegate.m
@@ -21,7 +21,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 //    self.window.rootViewController = [[ViewController alloc]init];
 //    [self.window makeKeyAndVisible];
-    NSLog(@"aaaaaaaa");
+    NSLog(@"aaaaaaaabbbbbbbbbbbbb");
     [[RootViewController sharedInstance]chooseRootViewControllerWithwindow:self.window];
     return YES;
 }
