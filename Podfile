f129dceba8c86a757e0f08eeac90e7adf3c514e3
杜文 <244834793@qq.com>
2018-07-18 16:01:21 +0800

    Masonry'

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios,'8.0'
+target 'YB-YH' do
+pod "Masonry"
+end
---
+++ Pods/Target Support Files/Pods-YB-YH/Pods-YB-YH-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YB_YH : NSObject
+@end
+@implementation PodsDummy_Pods_YB_YH
+@end
