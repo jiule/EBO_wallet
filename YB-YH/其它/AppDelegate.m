17512b6d950ae8442a8acd5fee8b33a21cd1d744
admin <admin@admindemac-mini.local>
2018-07-19 10:05:56 +0800

    shishi

--- YB-YH/其它/AppDelegate.m
+++ YB-YH/其它/AppDelegate.m
@@ -22,7 +22,7 @@
 //    [self.window makeKeyAndVisible];
     NSLog(@"aaaaaaaabbbbbbbbbbbbbdddddddd交换机1234444");
     NSLog(@"丹丹");
-    NSLog(@"aaaaaaaabbbbbbbbbbbbbdddddddd交换机1234444丹丹");
+    NSLog(@"aaaaaaaabbbbbbbbbbbbbdddddddd交换机");
     [[RootViewController sharedInstance]chooseRootViewControllerWithwindow:self.window];
     return YES;
 }
