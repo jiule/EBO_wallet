56734aa4316c7f8ae96d2cf1bc02545ff80267dd
Apple <244834793@qq.com>
2018-07-19 10:11:38 +0800

    提交

--- YB-YH/其它/AppDelegate.m
+++ YB-YH/其它/AppDelegate.m
@@ -21,7 +21,7 @@
 //    self.window.rootViewController = [[ViewController alloc]init];
 //    [self.window makeKeyAndVisible];
     NSLog(@"aaaaaaaabbbbbbbbbbbbbdddddddd交换机1234444");
-    NSLog(@"丹丹");
+    NSLog(@"丹丹1"); 
     NSLog(@"aaaaaaaabbbbbbbbbbbbbdddddddd交换机1234444丹丹");
     [[RootViewController sharedInstance]chooseRootViewControllerWithwindow:self.window];
     return YES;
